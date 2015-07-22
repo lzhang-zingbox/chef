6c90fde3675cf99a75f39aae1755b9680603ee04
lin <lzhang@zingbox.com>
2015-07-22 16:40:36 -0700

    add instance id

--- script/recipes/default.rb
+++ script/recipes/default.rb
@@ -1,10 +1,21 @@
-bash "cluster_test" do
+bash "add test file" do
   user "root"
   cwd "/tmp"
   code <<-EOH
     touch hello_world
+    mkdir -p 
   EOH
   not_if do
     File.exists?("/tmp/hello_world")
+  end
+end
+
+bash "generate instance id file" do
+  user "root"
+  cwd "/var/lib"
+  mkdir zookeeper
+  hostname|cut -b 3 > myid
+  not_if do
+    File.exists("/var/lib/zookeeper/myid")
   end
 end
