03971667d80fa68488b2c22345f66fb567b8aae7
lin <lzhang@zingbox.com>
2015-07-27 10:18:13 -0700

    update scirpt

--- script/recipes/deploy.rb
+++ script/recipes/deploy.rb
@@ -9,5 +9,6 @@ bash "update conf file" do
     echo "syncLimit=2" >> /opt/zookeeper/zookeeper-3.4.6/conf/zoo.cfg
     aws s3 sync s3://instancesfolder/ /tmp
     cat zk* >> /opt/zookeeper/zookeeper-3.4.6/conf/zoo.cfg
+    sudo kill -9 `ps afx |grep java |grep Dzookeeper|awk '{print $1;}'`
   EOH
 end
